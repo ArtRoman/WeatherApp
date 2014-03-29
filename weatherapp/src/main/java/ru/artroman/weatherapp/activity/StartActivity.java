7450a531857df51bdf23f6a2e79e17c9ac166715
artroman <artroman.developer@gmail.com>
2014-03-30 00:10:57 +0400

    Added SwipeRefreshLayout

--- weatherapp/src/main/java/ru/artroman/weatherapp/activity/StartActivity.java
+++ weatherapp/src/main/java/ru/artroman/weatherapp/activity/StartActivity.java
@@ -1,21 +1,16 @@
 package ru.artroman.weatherapp.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 import ru.artroman.weatherapp.R;
+import ru.artroman.weatherapp.fragment.MainContentFragment;
 import ru.artroman.weatherapp.fragment.NavigationDrawerFragment;
 
 
@@ -38,10 +33,10 @@ public class StartActivity extends ActionBarActivity implements NavigationDrawer
 
 	@Override
 	public void onNavigationDrawerItemSelected(int position) {
-		// update the main content by replacing fragments
+		// Update the main content by replacing fragments
 		FragmentManager fragmentManager = getSupportFragmentManager();
 		FragmentTransaction transaction = fragmentManager.beginTransaction();
-		transaction.replace(R.id.container, PlaceholderFragment.newInstance(position + 1));
+		transaction.replace(R.id.container, MainContentFragment.newInstance(position + 1));
 		transaction.commit();
 	}
 
@@ -89,42 +84,5 @@ public class StartActivity extends ActionBarActivity implements NavigationDrawer
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
 		actionBar.setDisplayShowTitleEnabled(true);
 		actionBar.setTitle(mTitle);
-	}
-
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		public PlaceholderFragment() {
-
-		}
-
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-			TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-			int selectedPage = getArguments().getInt(ARG_SECTION_NUMBER);
-			String textToDisplay = "Selected chapter: " + selectedPage;
-			textView.setText(textToDisplay);
-			return rootView;
-		}
-
-		@Override
-		public void onAttach(Activity activity) {
-			super.onAttach(activity);
-			((StartActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
-		}
 	}
 }
---
+++ weatherapp/src/main/java/ru/artroman/weatherapp/fragment/MainContentFragment.java
@@ -0,0 +1,84 @@
+package ru.artroman.weatherapp.fragment;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import ru.artroman.weatherapp.R;
+import ru.artroman.weatherapp.activity.StartActivity;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainContentFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+
+	private static final String ARG_SECTION_NUMBER = "section_number";
+	private static SwipeRefreshLayout mSwipeRefreshLayout;
+
+	public MainContentFragment() {
+
+	}
+
+	public static MainContentFragment newInstance(int sectionNumber) {
+		MainContentFragment fragment = new MainContentFragment();
+		Bundle args = new Bundle();
+		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	@Override
+	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+		if (rootView == null) return null;
+
+		TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+
+		int selectedPage = getArguments().getInt(ARG_SECTION_NUMBER);
+		String textToDisplay = "Selected chapter: " + selectedPage;
+		textView.setText(textToDisplay);
+
+		// Refresh layout
+		mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_refresh_container);
+		mSwipeRefreshLayout.setColorScheme(android.R.color.holo_blue_bright, android.R.color.holo_green_light, android.R.color.holo_orange_light, android.R.color.holo_red_light);
+		mSwipeRefreshLayout.setOnRefreshListener(this);
+
+		return rootView;
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		((StartActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+	}
+
+	@Override
+	public void onRefresh() {
+
+		Toast.makeText(getActivity(), "Refreshing", Toast.LENGTH_SHORT).show();
+
+		if (mSwipeRefreshLayout != null) {
+			mSwipeRefreshLayout.setRefreshing(true);
+		}
+
+		new Handler().postDelayed(stopRefreshDelayed, 2000);
+	}
+
+	private Runnable stopRefreshDelayed = new Runnable() {
+		@Override
+		public void run() {
+			if (mSwipeRefreshLayout != null) {
+				mSwipeRefreshLayout.setRefreshing(false);
+			}
+		}
+	};
+}
